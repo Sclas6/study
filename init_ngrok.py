10052c37989a377f9c31eee7c7bdbd87ff9f7a6a
Sclas6 <nagai.yuta.19@shizuoka.ac.jp>
2023-09-06 16:06:02 +0900

    Update 0.4.1

--- init_ngrok.py
+++
@@ -1,8 +0,0 @@
-import os
-
-from pyngrok import ngrok
-from pyngrok.conf import PyngrokConfig
-
-os.system('kill -9 $(pgrep ngrok)')
-webhook_url = ngrok.connect(addr='127.0.0.1:8080', pyngrok_config=PyngrokConfig(start_new_session=True))
-print (webhook_url)
